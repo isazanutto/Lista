339f58a11f879177d18f95f4def15d554fc00f96
isazanutto <43072267+isazanutto@users.noreply.github.com>
2018-12-02 20:21:25 -0200

    14

---
+++ posicao/posicao/posicao.cpp
@@ -0,0 +1,38 @@
+// posicao.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	char texto [8] = "Isabela";
+
+	printf("Valor dos vetores:\n",texto);
+
+	printf("texto[0]= %c\n",texto [0]);
+	printf("texto[1]= %c\n", texto[1]);
+	printf("texto[2]= %c\n", texto[2]);
+	printf("texto[3]= %c\n", texto[3]);
+	printf("texto[4]= %c\n", texto[4]);
+	printf("texto[5]= %c\n", texto[5]);
+	printf("texto[6]= %c\n", texto[6]);
+	
+
+	
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
