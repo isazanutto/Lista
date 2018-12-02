b8d4431dfb01c3a19bbc4bf5674df63df649c49f
isazanutto <43072267+isazanutto@users.noreply.github.com>
2018-12-02 02:14:29 -0200

    10

---
+++ potencia/potencia/potencia.cpp
@@ -0,0 +1,30 @@
+// potencia.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	float x, n,p;
+
+	printf("Digite a base da sua potencia:");
+	scanf_s("%f",&x);
+	printf("Digite o expoente:");
+	scanf_s("%f",&n);
+
+	p = pow(x,n);
+	printf("Resultado= %.2f",p);
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
